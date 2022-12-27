6d6d49da33fe19b32c9af50d7a52e64050872a69
codercyh <codercyh@163.com>
2022-12-27 16:17:00 +0800

    2022-12-27

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <Headers></Headers>
+    我是中间
+    <Footers></Footers>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Headers from './components/Header'
+import Footers from './components/Footer'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  components:{
+    Headers,
+    Footers
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
 </style>
